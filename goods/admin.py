2367100f24fd159681b17877b8879c82b29fbe90
lepeha <nostresko@gmail.com>
2024-04-25 13:28:30 +0400

    сохранения

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from goods.models import Categories, Product
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from goods.models import Categories
 
 def index(request):
+
+    categories = Categories.objects.all
+
     context = {
         'title': 'Home-главная',
         'content': 'магазин',
+        'categories': categories
     }
     return render(request, 'main/index.html', context)
 def about(request):
